3aa6106e86315c9d186d71a645e4213c65fb30e9
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-09-18 23:01:45 +0900

    Add public/$addon too

--- addon/ensure_symlinks.pl
+++ addon/ensure_symlinks.pl
@@ -13,10 +13,10 @@ use autodie;
   my $addonDir = dirname(File::Spec->rel2abs(__FILE__));
   my $appDir = dirname($addonDir);
   chdir($addonDir);
+  my @addon = grep {-d and -d "$_/public" and -d "$_/lib"} glob("*");
   my (@dir, %dir);
   my (%mod);
-  foreach my $addon (glob("*")) {
-    next unless -d $addon;
+  foreach my $addon (@addon) {
     chdir($addon);
     find(sub {
       return if $_ eq '.';
@@ -39,11 +39,19 @@ use autodie;
   # print join("\n", @dir), "\n";
   make_path(@dir, {verbose => 1});
 
+  foreach my $addon (@addon) {
+    my $srcFn = "addon/$addon/public";
+    my $dstFn = "public/$addon";
+    next if -l $dstFn and -r $dstFn;
+    system "ln", "-vnsfr", $srcFn, $dstFn;
+  }
+
   foreach my $addon (keys %mod) {
     my @mod = @{$mod{$addon}[1]};
     foreach my $fn (@mod) {
       my $srcFn = "addon/$addon/$fn";
       my $dstFn = $fn;
+      next if -l $dstFn and -r $dstFn;
       system "ln", "-vnsfr", $srcFn, $dstFn;
     }
   }
